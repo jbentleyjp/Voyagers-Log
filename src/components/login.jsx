eb209be1a54055a7f2819cee5c1abcbd9cfffd00
Jeff Bentley <jbentley449@gmail.com>
2020-05-13 00:38:30 +0900

    added some css changes, attempted modals

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+// import LoginModal from './modals/loginmodal'
 
 
-export default function SignUp () {
-
-
-
-
-
-    return (
-        <div>
-            <a id="login">Login</a>            
-        </div>
-    )
+export default function Login () {
+  return (
+    <div>
+       <div
+      id="Login"
+      // onClick={() => {
+      //   LoginModal();
+      //   }}
+        >Login
+      </div>      
+    </div>
+  )
 }
+
+                    
---
+++ src/components/modals/loginmodal.jsx
@@ -0,0 +1,54 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const loginModal = (
+//   <div className="ui dimmer modals visible active">  
+//     <div className="ui standard modal visible active">
+//       <h3>Login</h3>
+//       <form id="login-form" >
+//         <div className="login-field">
+//           <input type="email" id="login-email" placeholder="Input your email"></input>
+//           <label for="login-email">Email address</label>
+//         </div>
+//         <div className="login-field">
+//           <input type="password" id="login-password" placeholder="Input your password"></input>
+//           <label for="login-password">Your password</label>                        
+//         </div>
+//         <button className="modal-button"></button>
+//       </form>
+//     </div>
+//   </div>
+// );
+
+
+// function LoginModal(props) {
+//   return ReactDOM.createPortal(loginModal, document.querySelector("#modal"));
+// }
+
+
+// export default LoginModal;
+
+/* <div id="responsive" class="modal hide fade" tabindex="-1" data-width="760">
+<div class="modal-header">
+</div>
+<div class="modal-body">
+  <div class="row-fluid">
+  <h3>Login</h3>
+    <form id="login-form" >
+      <div className="login-field">
+        <input type="email" id="login-email" placeholder="Input your email"></input>
+        <label for="login-email">Email address</label>
+      </div>
+      <div className="login-field">
+        <input type="password" id="login-password" placeholder="Input your password"></input>
+        <label for="login-password">Your password</label>                        
+      </div>
+      <button className="modal-button"></button>
+    </form>
+  </div>
+</div>
+<div class="modal-footer">
+  <button type="button" data-dismiss="modal" class="btn">Close</button>
+  <button type="button" class="btn btn-primary">Save changes</button>
+</div>
+</div> */
