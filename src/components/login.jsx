853dff91f161b2a144791922f128d7ab8c5e011b
Jeff Bentley <jbentley449@gmail.com>
2020-05-13 01:34:27 +0900

    got a modal working

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-// import LoginModal from './modals/loginmodal'
+import React, { useState } from 'react';
+import LoginModal from './modals/loginmodal'
 
 
 export default function Login () {
+
+const [showModal, setShowModal] = useState(false)
+
   return (
     <div>
        <div
       id="Login"
-      // onClick={() => {
-      //   LoginModal();
-      //   }}
-        >Login
-      </div>      
+      onClick={() => {
+        setShowModal(true)
+      }}      
+      >Login
+      { showModal ? <LoginModal /> : null }   
+      </div>
     </div>
   )
 }
--- src/components/modals/loginmodal.jsx
+++ src/components/modals/loginmodal.jsx
@@ -1,32 +1,34 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import '../compstyles/modals.css'
 
-// const loginModal = (
-//   <div className="ui dimmer modals visible active">  
-//     <div className="ui standard modal visible active">
-//       <h3>Login</h3>
-//       <form id="login-form" >
-//         <div className="login-field">
-//           <input type="email" id="login-email" placeholder="Input your email"></input>
-//           <label for="login-email">Email address</label>
-//         </div>
-//         <div className="login-field">
-//           <input type="password" id="login-password" placeholder="Input your password"></input>
-//           <label for="login-password">Your password</label>                        
-//         </div>
-//         <button className="modal-button"></button>
-//       </form>
-//     </div>
-//   </div>
-// );
-
-
-// function LoginModal(props) {
-//   return ReactDOM.createPortal(loginModal, document.querySelector("#modal"));
-// }
+const loginModal = (
+  <div id="LoginModal" className="modal">
+    <div className="modal-content">
+      <h3>Login</h3>
+      <form id="login-form" >
+        <div className="login-field">
+          <input type="email" id="login-email" placeholder="Input your email"></input>
+          <label for="login-email">Email address</label>
+        </div>
+        <div className="login-field">
+          <input type="password" id="login-password" placeholder="Input your password"></input>
+          <label for="login-password">Your password</label>                        
+        </div>
+        <button type="button" className="modal-button">Submit</button>
+      </form>
+      </div> 
+  </div>
+ 
+);
 
 
-// export default LoginModal;
+function LoginModal(props) {
+  return ReactDOM.createPortal(loginModal, document.body);
+}
+
+
+export default LoginModal;
 
 /* <div id="responsive" class="modal hide fade" tabindex="-1" data-width="760">
 <div class="modal-header">
